d16e4b32d1811a151fbd2f7387807ba4196e8a48
Juan Hernandez <jarceluz@gmail.com>
2018-10-05 14:09:48 -0700

    primer Ejercicio de la unidad 3

---
+++ Unidad3/U3-1/U3-1/Coleccion.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace U3_1
+{
+    public class Coleccion:CollectionBase
+    {
+        public void Add(Object item)
+        {
+            InnerList.Add(item);
+        }
+
+        public void Remove(Object item)
+        {
+            InnerList.Remove(item);
+        }
+
+        public new void Clear()
+        {
+            InnerList.Clear();
+            
+        }
+
+        public new int Count()
+        {
+            return InnerList.Count;
+        }
+    }
+}
---
+++ Unidad3/U3-1/U3-1/_ViewPersona.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace U3_1
+{
+    public class _ViewPersona
+    {
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+    }
+}
